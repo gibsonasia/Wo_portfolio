c4d7642aa7d1c1733905c9c96b4ca2e91d67d0a9
Asia Gibson <gibsonjasia@gmail.com>
2022-01-29 22:23:43 -0500

    updated home page with background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../style/home.css"
+import background from "../components/img/wo2.jpg"
 
 const Home = () => {
     return(
-        <div>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <h1 className="head"> HOME</h1>
+        
+        <div >
+            <img src={background} className="bg"/>
+            <h1 className="headline">Wo Westone Potograpy</h1>
         </div>
+       
     );
 };
 
